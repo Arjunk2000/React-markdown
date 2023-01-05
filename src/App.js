fc9a37ee86e97445420b4e759cbae01672a90a4d
Arjunk2000 <96836318+Arjunk2000@users.noreply.github.com>
2023-01-05 13:02:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import React from "react";
+import LiveMarkdown from "./Components/LiveMarkdown";
+function App() {
+  return (
+    <div>
+      <LiveMarkdown />
+    </div>
+  );
+}
+export default App;
